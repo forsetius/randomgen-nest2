35df2541b57846b0aca5c0d0bbc676214ff6a8ba
Marcin Paździora <forseti.pl@gmail.com>
2025-10-09 02:24:10 +0200

    Validation of '/' endpoint
    - added `/` endpoint validation, 
    - added `/:lang endpoint, 
    - modified `LangDto` so that lang could be provided from multiple sources

--- app/src/shared/validation/LangDto.ts
+++ app/src/shared/validation/LangDto.ts
@@ -5,9 +5,14 @@ import z from 'zod';
 export const LangSchema = (config: AppConfigService) => {
   const defaultLang = config.getInferred('app.defaultLanguage');
 
-  return z.object().extend({
+  return z.object({
     lang: z.enum(Lang).default(defaultLang),
   });
 };
 
+export const OptionalLangSchema = z.object({
+  lang: z.enum(Lang).optional(),
+});
+
 export type LangDto = z.infer<ReturnType<typeof LangSchema>>;
+export type OptionalLangDto = z.infer<typeof OptionalLangSchema>;
